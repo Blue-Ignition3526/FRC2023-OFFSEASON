f30b693795f18a4f059996816250332a5533d2e4
rohm95 <raulmorales9521@gmail.com>
2023-09-05 10:52:23 -0500

    NO JALAN LOS PRESETS

--- src/main/java/frc/robot/commands/armCommands/UpdateArmPosition.java
+++ src/main/java/frc/robot/commands/armCommands/UpdateArmPosition.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.armCommands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Arm;
@@ -8,25 +9,34 @@ public class UpdateArmPosition extends CommandBase {
 
   private final Arm m_Brazo;
   private final double angle;
-  private final double direction;
+  private double direction;
   private double difference;
+  private boolean isUp;
 
   public UpdateArmPosition(Arm m_Brazo, double angle) {
     this.m_Brazo = m_Brazo;
     this.angle = angle;
     this.difference = angle - m_Brazo.getArmAngle();
-    this.direction = difference > 0 ? Constants.Robot.kArmUpSpeed : Constants.Robot.kArmDownSpeed;
     addRequirements(m_Brazo);
   }
 
   @Override
   public void initialize() {
+    this.isUp = (difference > 0.0);
+    if (isUp) {
+      this.direction = Constants.Robot.kArmUpSpeed;
+    } else if (!isUp) {
+      this.direction = Constants.Robot.kArmDownSpeed;
+    }
   }
 
   @Override
   public void execute() {
     m_Brazo.setMotors(direction);
     difference = angle - m_Brazo.getArmAngle();
+    SmartDashboard.putBoolean("IsUp", isUp);
+    SmartDashboard.putNumber("Direction", direction);
+    SmartDashboard.putNumber("Difference", difference);
   }
 
   @Override
